9c34500233760da84f19038e6775de251130b7ef
Victor Emiliano <vickb11k3@gmail.com>
2024-11-06 19:02:34 -0600

    commit final servicios terminados

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ class DevelopmentConfig():
     MYSQL_HOST='127.0.0.1'
     MYSQL_USER='root'
     MYSQL_PASSWORD=''
-    MYSQL_DB='api_utl'
+    MYSQL_DB='api'
     
 config={
     'development':DevelopmentConfig
